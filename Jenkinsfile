393e0c4a254df8f05675791c2a42dee10d065453
Andres <anpaezros@gmail.com>
2020-11-20 14:52:13 +0100

    Added load test

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,7 +29,11 @@ pipeline {
         }
         stage("Load Testing") {
             steps {
-                echo "Load Test..."
+                echo 'Installing k6'
+                sh 'sudo chmod +x setup_k6.sh'
+                sh 'sudo ./setup_k6.sh'
+                echo 'Running K6 performance tests...'
+                sh 'k6 run loadtests/performance-test.js'
             }
         }
     }
---
+++ loadtests/performance-test.js
@@ -0,0 +1,21 @@
+export let options = {
+    stages: [
+      { duration: '1m', target: 10 }, // simulate ramp-up of traffic from 1 to 10 users over 1 minutes.
+      { duration: '1m', target: 10 }, // stay at 10 users for 10 minutes
+      { duration: '1m', target: 20 }, // simulate ramp-up of traffic from 1 to 10 users over 1 minutes.
+      { duration: '1m', target: 20 },
+      { duration: '1m', target: 30 }, // simulate ramp-up of traffic from 1 to 10 users over 1 minutes.
+      { duration: '1m', target: 30 },
+      { duration: '1m', target: 0 }, // ramp-down to 0 users
+    ],
+    thresholds: {
+      http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+      'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+  };
+  
+  export default () => {
+    http.get('http://46.101.147.140:8080/hola');
+    sleep(1);
+  };
+  
